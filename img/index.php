cc1927b16e92712b595c90f614f80d105599a75a
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2019-12-07 12:13:04 -0700

    Initial Commit

---
+++ img/index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>All Community Images</title>
+		<link rel="icon" href="../favicon.ico" type="image/x-icon">
+	</head>
+	<body>
+		<h1>
+			All Community Images
+		</h1>
+		<p>Huntington Post reserves the right to remove any picture for any reason without any notice.<br/>Although we probably won't, this is just so if we do, it isn't a surprise.</p>
+		<?php
+		$images = glob("*"); //works in Rubular
+		foreach($images as $file) {
+			if($file=="index.php") continue;
+        //atm I don't know how to sort by date uploaded
+				echo '<a href="'.$file.'" style="margin:5px;"><img src="'.$file.'" height="200px"></a>';
+		}
+		?>
+	</body>
+</html>
